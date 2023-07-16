637b5d3f6a329316b53280b8f672c6edbfd000a5
Uzair Aftab <uzaaft@outlook.com>
2023-07-16 10:25:37 +0200

    chore: Refactor code, and make sure that errors exit gracefully

--- src/git_operations.rs
+++ src/git_operations.rs
@@ -1,6 +1,8 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use itertools::Itertools;
+use regex::Regex;
 use serde::Deserialize;
+use std::process::Command;
 
 static GITHUB_API_TREE_RECURSIVE: &str =
     "https://api.github.com/repos/AstroNvim/astrocommunity/git/trees/HEAD?recursive=1";
@@ -8,14 +10,16 @@ static GITHUB_API_TREE_RECURSIVE: &str =
 static GITHUB_API_TREE: &str =
     "https://api.github.com/repos/AstroNvim/astrocommunity/git/trees/HEAD";
 
-#[derive(Debug, Deserialize)]
-struct RepoContent {
-    path: String,
-}
+const REPO_PATH_PREFIX: &str = "lua/astrocommunity/";
 
 #[derive(Debug, Deserialize)]
 struct Tree {
     tree: Vec<RepoContent>,
+}
+
+#[derive(Debug, Deserialize)]
+struct RepoContent {
+    path: String,
 }
 
 #[derive(Debug, Clone)]
@@ -24,50 +28,55 @@ pub(crate) struct PluginInfo {
     pub name: String,
     pub fzf_string: String,
 }
+
 pub struct GitOperations;
+
 impl GitOperations {
     pub fn new() -> Self {
         Self
     }
+
     pub(crate) fn get_astrocommunity_tree(&self) -> Result<Vec<PluginInfo>> {
-        let tree = std::process::Command::new("curl")
+        let output = Command::new("curl")
             .arg(GITHUB_API_TREE_RECURSIVE)
-            .output()
-            .expect("failed to execute process");
-        Self::parse_response(tree.stdout)
+            .output()?;
+
+        if !output.status.success() {
+            Err(anyhow!(
+                "Curl command failed with exit code {:?}",
+                output.status.code()
+            ))
+        } else {
+            Self::parse_response(output.stdout)
+        }
     }
 
-    pub(crate) fn parse_response(response: Vec<u8>) -> Result<Vec<PluginInfo>> {
-    let tree: Tree = serde_json::from_slice(&response)?;
-    let re = regex::Regex::new(r"/[^/]+$").unwrap();
-    let plugin_paths = tree
-        .tree
-        .iter()
-        .map(|path| {
-            re.replace(&path.path, "")
-                .replace("lua/astrocommunity/", "")
-        })
-        .unique()
-        // Filtering edge cases
-        .filter(|p| !p.contains(".github") && p != "lua/astrocommunity")
-        .collect::<Vec<_>>();
+    fn parse_response(response: Vec<u8>) -> Result<Vec<PluginInfo>> {
+        let tree: Tree = serde_json::from_slice(&response)?;
+        let re = Regex::new(r"/[^/]+$")?;
 
-    let unique_plugins = plugin_paths
-        .iter()
-        .map(|path| 
-            // Split the path into a vector of strings
-            path.split('/').collect::<Vec<_>>()
-        )
-        .filter(|p| p.len() >= 2)
-        .map(|p| PluginInfo {
-            group: p[0].to_string(),
-            name: p[1].to_string(),
-            fzf_string: format!("{} [{}]", p[1], p[0]),
-        })
-        .collect::<Vec<_>>();
-    Ok(unique_plugins)
+        let unique_plugins = tree
+            .tree
+            .iter()
+            .map(|path| re.replace(&path.path, "").replace(REPO_PATH_PREFIX, ""))
+            .unique()
+            .filter(|p| !p.contains(".github") && p != REPO_PATH_PREFIX)
+            .filter_map(Self::parse_plugin_info)
+            .collect();
+
+        Ok(unique_plugins)
+    }
+
+    fn parse_plugin_info(path: String) -> Option<PluginInfo> {
+        let p: Vec<&str> = path.split('/').collect();
+        if p.len() >= 2 {
+            Some(PluginInfo {
+                group: p[0].to_string(),
+                name: p[1].to_string(),
+                fzf_string: format!("{} [{}]", p[1], p[0]),
+            })
+        } else {
+            None
+        }
+    }
 }
-}
-
-
-
