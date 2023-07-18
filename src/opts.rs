f52d4d2aa3106b1225860b9850c8d2abc67218ec
Uzair Aftab <uzaaft@outlook.com>
2023-07-18 18:00:31 +0200

    feat: Add command to generate the folder boilerplate for a new plugin

--- src/opts.rs
+++ src/opts.rs
@@ -1,7 +1,12 @@
+use std::{env, path::PathBuf};
+
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
 
-use crate::util::{copy_to_clipboard, print_with_syntax};
+use crate::{
+    astrocommunity::{self, Astrocommunity},
+    util::{copy_to_clipboard, print_with_syntax},
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -11,9 +16,6 @@ pub struct Cli {
 
     #[arg(short, long)]
     pub output: bool,
-
-    #[arg(short, long)]
-    pub unroll: bool,
 
     #[command(subcommand)]
     pub commands: Option<Commands>,
@@ -31,18 +33,56 @@ impl Cli {
             print_with_syntax(import_statement)
         }
     }
+
+    /// Create a new folder in atrocommunity directory
+    /// The folder should be:
+    pub fn create_new_plugin(&self) {
+        let astrocommunity_dir =
+            astrocommunity::Astrocommunity::find_astrocommunity_folder().unwrap();
+
+        let appname = env::var("NVIM_APPNAME").unwrap_or("nvim".to_string());
+
+        let args = self.commands.as_ref().unwrap().clone();
+        match args {
+            Commands::New {
+                astrocommunity_path,
+                group,
+                name,
+            } => {
+                let new_plugin_dir = PathBuf::from(astrocommunity_path).join(group).join(name);
+                std::fs::create_dir_all(&new_plugin_dir).unwrap();
+                let new_plugin_file = new_plugin_dir.join("init.lua");
+                let new_plugin_readme = new_plugin_dir.join("README.md");
+                std::fs::write(new_plugin_file, "").unwrap();
+                std::fs::write(new_plugin_readme, "").unwrap();
+                println!("Created new plugin at {}", new_plugin_dir.to_str().unwrap());
+            }
+        }
+    }
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 pub enum Commands {
-    // Create a new astrocommunity plugin
-    // New(NewArgs),
+    /// Create a new astrocommunity plugin
+    New {
+        /// The path for your astrocommunity path
+        #[arg(required = true)]
+        astrocommunity_path: String,
+        /// The group name of the plugin. Example: pack, editor-support, etc
+        #[arg(required = true)]
+        group: String,
+        /// The name of the plugin. Could be the name of the pack, or the name of the plugin
+        #[arg(required = true)]
+        name: String,
+    },
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 pub struct NewArgs {
-    pub group: Option<String>,
-    pub name: Option<String>,
+    /// The group name of the plugin. Example: pack, editor-support, etc
+    pub group: String,
+    /// The name of the plugin. Could be the name of the pack, or the name of the plugin
+    pub name: String,
 }
 pub fn get_opts() -> Cli {
     Cli::parse()
